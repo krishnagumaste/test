d1dc84405e316a8d848627a85578ebf0262280ff
krishnagumaste <krishnagumaste513@gmail.com>
2024-08-22 11:11:47 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Example from './components/Example';
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Example />
+    </div>
+    
+  )
+}
