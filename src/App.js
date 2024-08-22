7662d587706e5444eae31644368190e4109264c7
krishnagumaste <krishnagumaste513@gmail.com>
2024-08-22 12:30:40 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Example from './components/Example';
 import Landing from './components/Landing';
-import NavBar from './components/NavBar';
 import Login from './components/Login';
 
 function App() {
-  const location = useLocation();
 
   return (
     <div>
-      {/* Render NavBar only if the current route is not '/login' */}
-      {location.pathname !== '/' && <NavBar />}
-      
       <Routes>
         <Route path="/example" element={<Example />} />
         <Route path="/landing" element={<Landing />} />
