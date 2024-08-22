0075be6ec96e5c6a1de0f016589322485726a9a2
krishnagumaste <krishnagumaste513@gmail.com>
2024-08-22 12:33:44 +0530

    commit

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Landing = () => {
+  const navigate = useNavigate();
   return (
-    <div>Landing</div>
+    <div>Landing
+      <button
+                  type="submit"
+                  onClick={() => navigate("/example")}
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Click
+                </button>
+    </div>
+    
   )
 }
 
